6d57ac451003b571707836015631cc96898de3ef
Emon <emon.mhk69@gmail.com>
2023-09-24 00:04:00 +0600

    desire compo used

---
+++ src/components/users/UserComponent.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const UserComponent = ({user}) => {
+    return (
+        <div className="shadow-md p-4 rounded bg-white">
+            <div className="">
+                <img
+                    src={user?.image ? user?.image : "https://i.ibb.co/5nPD3Qg/user.jpg"}
+                    alt="user image"
+                    className="h-40 w-40 rounded mx-auto"
+                />
+            </div>
+            <div className="">
+                <h2 className="text-2xl font-semibold">{user?.name}</h2>
+                <p>
+                    <span className="font-semibold">EMAIL: </span> {user?.email ? user?.email : "N/A"}
+                </p>
+                <p>
+                    <span className="font-semibold">AGE: </span> {user?.age ? user?.age : "N/A"}
+                </p>
+            </div>
+            <div className="flex justify-end">
+                <Link to={`/users/${user._id}`}>
+                    <button className="font-semibold text-white bg-violet-600 px-2 py-1">
+                        View Details
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default UserComponent;
--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import SectionTitle from "../../shared/SectionTitle";
 import CSkeleton from "../../components/customComponent/CSkeleton";
 import { useGetAllUsersQuery } from "../../redux/features/users/user-api-slice";
+import UserComponent from "../../components/users/UserComponent";
 
 const Users = () => {
 
@@ -25,31 +26,7 @@ const Users = () => {
             <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 p-2">
                 {
                     users ?
-                        users.map((user, index) => <div key={index} className="shadow-md p-4 rounded bg-white">
-                            <div className="">
-                                <img
-                                    src={user?.image ? user?.image : "https://i.ibb.co/5nPD3Qg/user.jpg"}
-                                    alt="user image"
-                                    className="h-40 w-40 rounded mx-auto"
-                                />
-                            </div>
-                            <div className="">
-                                <h2 className="text-2xl font-semibold">{user?.name}</h2>
-                                <p>
-                                    <span className="font-semibold">EMAIL: </span> {user?.email ? user?.email : "N/A"}
-                                </p>
-                                <p>
-                                    <span className="font-semibold">AGE: </span> {user?.age ? user?.age : "N/A"}
-                                </p>
-                            </div>
-                            <div className="flex justify-end">
-                                <Link to={`/users/${user._id}`}>
-                                    <button className="font-semibold text-white bg-violet-600 px-2 py-1">
-                                        View Details
-                                    </button>
-                                </Link>
-                            </div>
-                        </div>)
+                        users.map((user, index) => <UserComponent key={index} user={user} />)
                         :
                         !isLoading &&
                         <h2 className='text-red-500  font-bold text-2xl text-center md:col-span-2 xl:col-span-3'>
