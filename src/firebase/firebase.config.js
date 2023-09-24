b775006974ef46a74dba1824def6642cac65bbd7
Emon <emon.mhk69@gmail.com>
2023-09-24 23:46:01 +0600

    firebase setup done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,18 +1,14 @@
-/* eslint-disable no-unused-vars */
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBg4UFM6KTawteAP14nCdpuGf235ZkDlXA",
-  authDomain: "webit-b26fc.firebaseapp.com",
-  projectId: "webit-b26fc",
-  storageBucket: "webit-b26fc.appspot.com",
-  messagingSenderId: "1049094068432",
-  appId: "1:1049094068432:web:235b17b3fa70b9a1d0a6d5"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+
+export const app = initializeApp(firebaseConfig);
